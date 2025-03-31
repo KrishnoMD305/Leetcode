29ddff35e8a4d2e9be5e8f8a6ca6f59b6958d72a
Krishna Mondal <krishnomd60@gmail.com>
2025-03-31 10:59:17 +0600

    Create Solution2.py

---
+++ 1392. Longest Happy Prefix/Solution2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPrefix(self, s: str) -> str:
+        n = len(s)
+        lps = [0]*n
+        i,j = 1,0
+        while i<n:
+            if s[i]==s[j]:
+                j+=1
+                lps[i] = j
+                i += 1
+            elif j>0:
+                j = lps[j-1]
+            else:
+                lps[i] = 0
+                i += 1
+        return s[:lps[-1]]
