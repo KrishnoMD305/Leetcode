be584c12fa78e8412e099dd4e98036b4107b7ec9
Krishna Mondal <krishnomd60@gmail.com>
2025-06-11 09:39:50 +0600

    Create Solution.cpp

---
+++ 231. Power of Two/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+        int x = log2(n);
+        if(pow(2,x)==n){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
