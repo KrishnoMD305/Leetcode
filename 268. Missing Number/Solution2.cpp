3705710a9ee7c859c660a8cdf58f4b1ea5caaa83
Krishna Mondal <krishnomd60@gmail.com>
2025-06-11 12:26:01 +0600

    Create Solution2.cpp

---
+++ 268. Missing Number/Solution2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int start=0, end=nums.size();
+        while(start<end){
+            int mid = (end+start)/2;
+            if(nums[mid]>mid){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+};
