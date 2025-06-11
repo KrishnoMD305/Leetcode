925040cc32951412dff88ab0ed7072edea430619
Krishna Mondal <krishnomd60@gmail.com>
2025-06-11 12:37:42 +0600

    Create Solution.py

---
+++ 350. Intersection of Two Arrays II/Solution.py
@@ -0,0 +1,15 @@
+# 1660482682
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = []
+        nums1.sort()
+        nums2.sort()
+        i,j = 0, 0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]==nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i]<nums2[j]: i += 1
+            else: j += 1
+        return result
